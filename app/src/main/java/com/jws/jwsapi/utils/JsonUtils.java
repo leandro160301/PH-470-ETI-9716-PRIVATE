c093647675847516aed0942bc06db6dbd71ed8a2
leandro160301 <128090420+leandro160301@users.noreply.github.com>
2024-09-25 13:57:52 -0300

    Commit autom├ítico

--- app/src/main/java/com/jws/jwsapi/utils/JsonUtils.java
+++ app/src/main/java/com/jws/jwsapi/utils/JsonUtils.java
@@ -67,6 +67,21 @@ public class JsonUtils {
         return resultados;
     }
 
+    public static List<String> getFieldFromJsonMatrix(String campo, String JSON, String Matriz) {
+        List<String> resultados = new ArrayList<>();
+        JsonElement jsonElement = JsonParser.parseString(JSON);
+        if (jsonElement.isJsonObject()) {
+            JsonObject jsonObject = jsonElement.getAsJsonObject();
+            if (jsonObject.has(Matriz)) {
+                JsonArray jsonArray = jsonObject.getAsJsonArray(Matriz);
+                for (JsonElement element : jsonArray) {
+                    getFieldFromElement(campo, resultados, jsonElement, element);
+                }
+            }
+        }
+        return resultados;
+    }
+
     private static void getFieldFromElement(String campo, List<String> resultados, JsonElement jsonElement, JsonElement element) {
         if (element.isJsonObject()) {
             JsonObject jsonObject = element.getAsJsonObject();
@@ -81,20 +96,5 @@ public class JsonUtils {
 
             }
         }
-    }
-
-    public static List<String> getFieldFromJsonMatrix(String campo, String JSON, String Matriz) {
-        List<String> resultados = new ArrayList<>();
-        JsonElement jsonElement = JsonParser.parseString(JSON);
-        if (jsonElement.isJsonObject()) {
-            JsonObject jsonObject = jsonElement.getAsJsonObject();
-            if (jsonObject.has(Matriz)) {
-                JsonArray jsonArray = jsonObject.getAsJsonArray(Matriz);
-                for (JsonElement element : jsonArray) {
-                    getFieldFromElement(campo, resultados, jsonElement, element);
-                }
-            }
-        }
-        return resultados;
     }
 }
